cb9b09b387c6f7883231767bc8aab840f3c201c4
chenghao <473726199@qq.com>
2023-07-12 15:41:10 +0800

    ⚡️perf: 优化export

---
+++ lib/export.dart
@@ -0,0 +1,3 @@
+export 'load_page.dart';
+export 'load_status.dart';
+export 'load_widget.dart';
--- lib/index.dart
+++
@@ -1,5 +0,0 @@
-library lib;
-
-export './load_page.dart';
-export './load_status.dart';
-export './load_widget.dart';
